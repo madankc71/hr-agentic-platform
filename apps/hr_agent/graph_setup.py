f6a3ed7487e40f80baa59d0bcfbf93bc2d8a86ac
Madan K C <madankc71@gmail.com>
2025-11-26 10:46:33 -0600

    feat: add payroll node with OpenAI integration and unit tests

--- apps/hr_agent/graph_setup.py
+++ apps/hr_agent/graph_setup.py
@@ -5,7 +5,7 @@ from .nodes.router_node import router_node
 from .nodes.policy_node import policy_node
 from .nodes.leave_node import leave_node
 from .nodes.benefits_node import benefits_node
-
+from .nodes.payroll_node import payroll_node
 
 workflow = StateGraph(HRState)
 
@@ -15,7 +15,7 @@ workflow.add_node("policy_node", policy_node)
 
 workflow.add_node("leave_node", leave_node)
 workflow.add_node("benefits_node", benefits_node)
-
+workflow.add_node("payroll_node", payroll_node)
 
 workflow.add_edge("classifier", "router")
 
